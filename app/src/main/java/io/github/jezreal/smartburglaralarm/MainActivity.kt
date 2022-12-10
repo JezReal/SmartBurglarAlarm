e4097178abd11d608fe8e3814efbae65fe7621d3
JezReal <jezreeljoshuam@gmail.com>
2022-12-10 21:56:55 +0800

    setup navigation

--- app/src/main/java/io/github/jezreal/smartburglaralarm/MainActivity.kt
+++ app/src/main/java/io/github/jezreal/smartburglaralarm/MainActivity.kt
@@ -3,11 +3,15 @@ package io.github.jezreal.smartburglaralarm
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
-import io.github.jezreal.smartburglaralarm.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     private val picker =
         MaterialTimePicker.Builder()
@@ -20,11 +24,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     fun selectFromTime(view: View) {
--- app/src/main/java/io/github/jezreal/smartburglaralarm/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/jezreal/smartburglaralarm/ui/main/MainFragment.kt
@@ -1,32 +1,35 @@
 package io.github.jezreal.smartburglaralarm.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.github.jezreal.smartburglaralarm.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import io.github.jezreal.smartburglaralarm.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
