434d80cc9ba7daffaacc1601e6bb65339ad813f7
JezReal <jezreeljoshuam@gmail.com>
2022-12-10 22:29:49 +0800

    move MainActivity.kt

--- app/src/main/java/io/github/jezreal/smartburglaralarm/ui/MainActivity.kt
+++ app/src/main/java/io/github/jezreal/smartburglaralarm/ui/MainActivity.kt
@@ -1,15 +1,16 @@
-package io.github.jezreal.smartburglaralarm
+package io.github.jezreal.smartburglaralarm.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import io.github.jezreal.smartburglaralarm.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
