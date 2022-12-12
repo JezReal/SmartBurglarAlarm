ee42458dea859e1b2be1e4e7eb9f29789526e37a
go-roii <royfgopez80@gmail.com>
2022-12-13 01:17:22 +0800

    add bottom nav and second fragment

--- app/src/main/java/io/github/jezreal/smartburglaralarm/ui/MainActivity.kt
+++ app/src/main/java/io/github/jezreal/smartburglaralarm/ui/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.jezreal.smartburglaralarm.R
 import io.github.jezreal.smartburglaralarm.databinding.ActivityMainBinding
@@ -25,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
 
 //        NavigationUI.setupActionBarWithNavController(this, navController)
     }
